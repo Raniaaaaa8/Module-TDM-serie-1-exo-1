bd5daf94c03be0c175438ce991e842939186018a
Rania Bouguesri <45489042+Raniaaaaa8@users.noreply.github.com>
2021-04-11 11:00:14 +0100

    Add files via upload

---
+++ src/main/java/com/example/ran/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ran.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import java.time.LocalDate
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
